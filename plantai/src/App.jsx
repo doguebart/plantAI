1d7e583a5700c1198d64bfef7db879d803da65d2
doguebart <douglasweelber.2004@outlook.com>
2023-10-20 14:47:06 -0300

    feat: responsive header

--- plantai/src/App.jsx
+++ plantai/src/App.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React from "react";
+import Menu from "./components/menu";
 
 const App = () => {
   return (
-    <div className='w-full h-screen bg-white'>
-
+    <div className="w-full h-screen bg-white">
+      <Menu />
     </div>
-  )
-}
+  );
+};
 
 export default App;
---
+++ plantai/src/components/menu/index.jsx
@@ -0,0 +1,86 @@
+import React, { useState } from "react";
+import { FiMenu } from "react-icons/fi";
+import { Link } from "react-scroll";
+import { animateScroll as scroll } from "react-scroll";
+
+const Menu = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  const scrollToTop = () => {
+    scroll.scrollToTop();
+  };
+
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
+
+  return (
+    <nav className="w-full p-4 bg-blue-500 md:flex md:justify-around md:items-center fixed top-0 z-10">
+      <div className="flex justify-between items-center max-w-full">
+        <Link to="/" onClick={scrollToTop} className="cursor-pointer">
+          <img
+            src="/plantailogo.png"
+            className="w-10 h-10 rounded-full shadow-md"
+            alt="Plant AI logo"
+          />
+        </Link>
+
+        <span
+          className="text-3xl cursor-pointer mx-2 md:hidden block"
+          onClick={toggleMenu}
+        >
+          <FiMenu className="text-white" />
+        </span>
+      </div>
+
+      <ul
+        className={`${
+          menuOpen ? "block" : "hidden"
+        } md:flex space-y-4 border-t mt-4 lg:mt-0 md:border-0 border-white/50 md:space-y-0 md:space-x-10 items-center md:bg-transparent md:p-0 py-6 md:w-auto md:opacity-100 transition-all ease-in duration-500`}
+      >
+        <li>
+          <Link
+            to="solucao"
+            spy={true}
+            smooth={true}
+            duration={500}
+            onClick={closeMenu}
+            className="text-white font-medium text-sm py-2 hover:border-b duration-200 whitespace-nowrap cursor-pointer"
+          >
+            Nossa Solução
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="parceiros"
+            spy={true}
+            smooth={true}
+            duration={500}
+            onClick={closeMenu}
+            className="text-white font-medium text-sm py-2 hover:border-b duration-200 whitespace-nowrap cursor-pointer"
+          >
+            Parceiros
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="quem-somos"
+            spy={true}
+            smooth={true}
+            duration={500}
+            onClick={closeMenu}
+            className="text-white font-medium text-sm py-2 hover:border-b duration-200 whitespace-nowrap cursor-pointer"
+          >
+            Quem Somos
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Menu;
